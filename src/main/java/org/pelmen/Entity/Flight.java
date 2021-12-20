d3e7e22b5c04111728ed106751b24dddb1328459
kvandr <95276060+kvandr@users.noreply.github.com>
2021-12-20 11:10:46 +0400

    All

--- src/main/java/org/pelmen/Entity/Flight.java
+++ src/main/java/org/pelmen/Entity/Flight.java
@@ -29,4 +29,44 @@ public class Flight {
         this.travelTime = travelTime;
     }
 
+    public Long getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(Long flightId) {
+        this.flightId = flightId;
+    }
+
+    public String getAirbus() {
+        return airbus;
+    }
+
+    public void setAirbus(String airbus) {
+        this.airbus = airbus;
+    }
+
+    public Long getRoute() {
+        return route;
+    }
+
+    public void setRoute(Long route) {
+        this.route = route;
+    }
+
+    public Date getDepartTime() {
+        return departTime;
+    }
+
+    public void setDepartTime(Date departTime) {
+        this.departTime = departTime;
+    }
+
+    public Date getTravelTime() {
+        return travelTime;
+    }
+
+    public void setTravelTime(Date travelTime) {
+        this.travelTime = travelTime;
+    }
+
 }
--- src/main/java/org/pelmen/Entity/Route.java
+++ src/main/java/org/pelmen/Entity/Route.java
@@ -1,15 +1,32 @@
 package org.pelmen.Entity;
 
-import lombok.Data;
-
-@Data
 public class Route {
-    private Long RouteId;
+    private Long routeId;
     private String departPoint;
     private String arrivalPoint;
 
     public Route(String departPoint, String arrivalPoint) {
         this.departPoint = departPoint;
+        this.arrivalPoint = arrivalPoint;
+    }
+    public Long getRouteId(){
+        return this.routeId;
+    }
+    public void setRouteId(Long routeId) {
+        this.routeId = routeId;
+    }
+
+    public String getDepartPoint(){
+        return this.departPoint;
+    }
+    public void setDepartPoint(String departPoint) {
+        this.departPoint = departPoint;
+    }
+
+    public String getArrivalPoint(){
+        return this.arrivalPoint;
+    }
+    public void setArrivalPoint(String arrivalPoint) {
         this.arrivalPoint = arrivalPoint;
     }
 }
